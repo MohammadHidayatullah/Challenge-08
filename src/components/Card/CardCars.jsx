13d2e540b58f0457d44921bcd94fe2d05dbb8dbd
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2022-06-09 22:47:08 +0700

    first commit

---
+++ src/components/Card/CardCars.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import User from '../../assets/icon/fi_users.svg'
+import Setting from '../../assets/icon/fi_settings.svg'
+import Calendar from '../../assets/icon/fi_calendar.svg'
+import { useDispatch, useSelector } from 'react-redux';
+import { getCar } from '../../redux/action/carAction';
+import { Link} from 'react-router-dom';
+import { handleCarByID } from '../../redux/action/carByIdAction';
+
+function CardCars() {
+    const dispatch = useDispatch();
+
+    const { isLoading: loadingCar, data: carData} = useSelector(
+        (state) => state.car
+    );
+
+    useEffect(() => {
+        dispatch(getCar());
+    }, []);
+
+    return (
+    <>
+        {loadingCar ? 'Loading' : carData?.map((item) => (
+        <div className="col-lg-4 col-md-6 col-12 mt-5 mx-0 d-flex justify-content-center">
+            <div className="card">
+                <div className="card-body">
+                    <img src={`${ item.image }`} className="showcase-img" alt="..." />
+                    <div className="car-name">{`${ item.name }`}/{`${ item.category }`}</div>
+                    <div className="car-price">Rp {`${ item.price }`} / hari</div>
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className="icon">
+                        <p class="card-text">
+                            <img className='me-1' src={User} alt="icon-key" />{`${ item.capacity }`} orang
+                        </p>
+                        <p class="card-text">
+                            <img className='me-1' src={Setting} alt="icon-clock" />{`${ item.transmission }`}
+                        </p>
+                        <p className="card-text">
+                            <img className='me-1' src={Calendar} alt="icon-clock" />Tahun {`${ item.year }`}
+                        </p>
+                    </div>
+                    <Link
+                        to={`/search/${item.id}`}
+                        className="btn btn-primary  w-100 choose-car d-flex justify-content-center align-items-center"
+                        onClick={() => dispatch(handleCarByID(item))}>
+                        Pilih Mobil
+                    </Link>
+                </div>
+            </div>
+        </div>
+        ))}
+    </>
+    )
+}
+
+export default CardCars
---
+++ src/redux/action/carAction.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { EDIT_CAR, GET_CAR } from "../types";
+
+export const getCar = () => {
+    return (dispatch) => {
+        dispatch({ type: `${GET_CAR}_LOADING`});
+
+        axios({
+            method: 'GET',
+            url: 'https://62601c3e53a42eaa0700492d.mockapi.io/api/v1/cars',
+            // url: 'https://rent-cars-api.herokuapp.com/admin/car',
+        })
+        .then((response) => {
+            dispatch({
+                type: `${GET_CAR}_FULFILLED`,
+                payload: response.data,
+              });
+        })
+        .catch((error) => {
+            dispatch({
+              type: `${GET_CAR}_ERROR`,
+              error: error.message,
+            });
+        });
+    };
+};
+
+export const editCard = (id, data) => {
+    return (dispatch) => {
+        dispatch({ type: `${EDIT_CAR}_LOADING` });
+
+        axios({
+            method: 'PUT',
+            url: `https://62601c3e53a42eaa0700492d.mockapi.io/api/v1/cars`,
+            // url: `https://rent-cars-api.herokuapp.com/admin/car`,
+            data,
+        })
+        .then(() => {
+            dispatch({
+              type: `${EDIT_CAR}_FULFILLED`,
+              // payload: response.data,
+            });
+            dispatch(getCar());
+        })
+        .catch((error) => {
+            dispatch({
+              type: `${EDIT_CAR}_ERROR`,
+              error: error.message,
+            });
+        });
+    };
+};
